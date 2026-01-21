e4f3729e6b358ae14937bf68aef529a9d285aa14
Rahul230111 <imrahuldravid23@gmail.com>
2026-01-21 16:26:28 +0530

    theme

--- src/layout/content/Content.js
+++ src/layout/content/Content.js
@@ -3,7 +3,7 @@ import React from "react";
 const Content = ({ ...props }) => {
   return (
     <div  className="nk-content">
-      <div className="container-fluid">
+      {/* <div className="container-fluid"> */}
         <div className="nk-content-inner">
           <div className="nk-content-body">
             {!props.page ? props.children : null}
@@ -11,7 +11,7 @@ const Content = ({ ...props }) => {
               <div className="components-preview wide-md mx-auto">{props.children}</div>
             ) : null}
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </div>
   );
